723593a6e9380f1fcc524b5aacfebb0f4705c525
Sleuthing <anaslaykah@yahoo.com>
2025-07-08 15:08:19 +0200

    Added Plotly interactive graphs with preview images

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import pandas as pd 
-# import streamlit as st
 import plotly.graph_objs as go
 import sys
+
+# to handle the output of some characters
 sys.stdout.reconfigure(encoding='utf-8')
 
-# sys.stdout.reconfigure(encoding='utf-8')
-# on_bad_lines='skip', encoding='utf-8')
-
-df = pd.read_csv('Bildung.csv', sep=';') 
+df = pd.read_csv('./Bildung.csv', sep=';') 
 
 # avoid NaNs in result
 cols = ['germans_male', 'foreigns_male', 'germans_female', 'foreigns_female']
@@ -20,63 +18,125 @@ df['total_students'] = (
   + df['foreigns_female']
 )
 
-df['total_students'] = (
-    df['germans_male']
-  + df['foreigns_male']
-  + df['germans_female']
-  + df['foreigns_female']
+####
+# task 1: in which wintersemester were there the most students?
+totals_students_by_ws = df.groupby('semester', as_index=True)['total_students'].sum()
+
+best_ws   = totals_students_by_ws.idxmax()
+max_count = totals_students_by_ws.max()
+
+fig = go.Figure()
+
+totals_students_by_ws = totals_students_by_ws.reset_index()
+
+fig.add_trace(go.Scatter(
+    x=totals_students_by_ws["semester"],
+    y=totals_students_by_ws["total_students"],
+    mode="lines+markers",
+    name="Total Students",
+    line=dict(color="blue")
+))
+
+fig.add_trace(go.Scatter(
+    x=[best_ws],
+    y=[max_count],
+    mode="markers+text",
+    # text=["⬆ Max"],
+    textposition="top center",
+    marker=dict(size=20, color="red", symbol="star"),
+    name="Most students per semester"
+))
+
+fig.update_layout(
+    title="Total Students per Semester",
+    xaxis_title="Semester",
+    yaxis_title="Total Students",
+    legend_title="Legend",
 )
 
-# print(df.columns.tolist())
-# winter = df[df['semester'].str.upper().str.startswith('WS', na=False)]
+fig.show()
 
-totals_by_ws = df.groupby('semester', as_index=True)['total_students'].sum()
+####
+# task 2: program with the most international students
 
-
-best_ws   = totals_by_ws.idxmax()
-max_count = totals_by_ws.max()
-
-print(f"The semester {best_ws} had the most students with a total of {max_count} students")
+fig = go.Figure()
 
 df['international_students'] = (
   + df['foreigns_male']
   + df['foreigns_female']
 )
 
-studienBereich = df.groupby('program')['total_students'].sum()
+studienBereich = df.groupby('program')['international_students'].sum()
 
 top_program         = studienBereich.idxmax()
 top_program_count   = int(studienBereich.max())
 
-print(f"Program with the most foreign students: {top_program!r}") 
-'''
-  the !r tells Python to call repr(value) instead of str(value). That means:
-str(value) → a “pretty” or user‑friendly string
-repr(value) → a string that, as much as possible, looks like valid Python code and shows quotes and escape sequences
-'''
-print(f" Total foreignstudent headcount: {top_program_count:,}")
-'''
- the :, is the format‐spec for thousand separators
-Inside the {} of an f‑string, anything after a : is a format spec. 
-Putting a comma in there tells Python to insert commas as thousands separators for numbers.
-'''
+studienBereich = studienBereich.reset_index()
 
-top5 = studienBereich.nlargest(5)
-print(top5)
+fig.add_trace(go.Scatter(
+    x=studienBereich['program'],
+    y=studienBereich['international_students'],
+    mode="lines+markers",
+    line=dict(color="blue"),
+    name="International students per semester"
+))
+
+fig.add_trace(go.Scatter(
+    x=[top_program],
+    y=[top_program_count],
+    mode="markers+text",
+    textposition="top center",
+    marker=dict(size=20, color="green", symbol="star"),
+    name="Most international students"
+))
+
+fig.update_layout(
+    title="Total International Students per Program",
+    xaxis_title="Program",
+    yaxis_title="Total International Students",
+    legend_title="Legend",
+)
+
+fig.show()
+
+####
+# task 3: the percentage of female students in the program with the most female students
+
+fig = go.Figure()
 
 df['total_female'] = df['germans_female']+df['foreigns_female']
 
 femaleMajority = df.groupby('program', as_index=True)[['total_female', 'total_students']].sum()
-femaleMajority['female_percentage'] = round(femaleMajority['total_female']/femaleMajority['total_students']*100, 2)
-# print(femaleMajority.max())
-# print(femaleMajority.idxmax())
-print(femaleMajority.sort_values('female_percentage',ascending=False))
-print(f" Total foreign students headcount: {top_program_count:,}")
-#df['female_percentage'] = df['total_female']+df['total_students']
+femaleMajority['female_percentage'] = femaleMajority['total_female']/femaleMajority['total_students']
 
-# top_female_program         = femaleMajority.idxmax()
-# top_female_program_count   = int(femaleMajority.max())
-# print(top_female_program)
-# print(top_female_program_count)
+max_female_perecentage = femaleMajority['female_percentage'].max()
+female_dominated_program = femaleMajority[femaleMajority['female_percentage'] == max_female_perecentage].reset_index()['program'].iloc[0]
 
+femaleMajority = femaleMajority.reset_index()
 
+fig.add_trace(go.Scatter(
+    x=femaleMajority['program'],
+    y=femaleMajority['female_percentage'],
+    mode="lines+markers",
+    line=dict(color="blue"),
+    name="Female pecentage per prgoram"
+))
+
+fig.add_trace(go.Scatter(
+    x=[female_dominated_program],
+    y=[max_female_perecentage],
+    mode="markers+text",
+    textposition="top center",
+    marker=dict(size=20, color="purple", symbol="star"),
+    name="Most female students"
+))
+
+fig.update_layout(
+    title="Percentage of female students per program",
+    xaxis_title="Program",
+    yaxis_title="Percentage",
+    yaxis_tickformat=".2%",
+    legend_title="Legend",
+)
+
+fig.show()
